056e730fd7d0042d69118f6990237ad1a7e0c481
ynwahid <yusufnurwahid.contact@gmail.com>
2022-05-24 19:47:38 +0700

    redis with config

--- command/cli.sh
+++ command/cli.sh
@@ -1,3 +1,6 @@
+# 1
+# Jalankan docker compose
+docker-compose -f "1. redis/docker-compose.yaml" up -d
 # Masuk ke shell container redis
 docker container exec -it redis /bin/sh
 # Masuk ke cli redis
@@ -7,3 +10,16 @@ redis-cli -h <host> -p <port>
 redis-cli -h localhost
 # Cek koneksi ke server redis
 ping
+# Matikan dan hapus container hasil docker compose
+docker-compose -f "1. redis/docker-compose.yaml" down
+
+# 2
+# Jalankan docker compose
+docker-compose -f "2. redis-with-config/docker-compose.yaml" up -d
+# Masuk ke redis di container
+docker container exec -it redis /bin/sh
+redis-cli -h localhost
+# Pilih database yang akan digunakan (default 0).
+select 1
+# Matikan dan hapus container hasil docker compose
+docker-compose -f "2. redis-with-config/docker-compose.yaml" down
